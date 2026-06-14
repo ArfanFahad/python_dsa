d9d61cc1c416cd47229a8f15679fb7114ab010f9
ArfanFahad <arfanfahad40@gmail.com>
2026-06-14 11:56:51 +0600

    matrix done

--- chapter-25/matrix_2nd.py
+++ chapter-25/matrix_2nd.py
@@ -38,22 +38,24 @@ print("Get second student's marks: ", scores[1])
 print("Get all marks in subject 1: ", scores[:,0])
 print("Get the marks at row 2 column 1: ", scores[1, 0])
 
-print("Average marks of each student:\n",
-      f"Student 1: {scores[0]}, Average: {scores[0].mean():.2f} \n"
-      f"Student 2: {scores[1]}, Average: {scores[1].mean():.2f} \n"
-      f"Student 3: {scores[2]}, Average: {scores[2].mean():.2f} \n"
-      f"Student 4: {scores[3]}, Average: {scores[3].mean():.2f} \n")
+#         axis=1 →
+#       ---------------->
+#        [85 90 88]
+# axis=0 [70 65 80]
+#    ↓   [95 92 96]
+#        [60 75 70]
 
-print("Average mark of each subject:\n",
-      f"Math marks: {scores[:,0]}, Average marks: {scores[:,0].mean():.2f} \n"
-      f"English marks: {scores[:,1]}, Average marks: {scores[:,1].mean():.2f} \n"
-      f"Physics marks: {scores[:,2]}, Average marks: {scores[:,2].mean():.2f} \n"
-      )
 
-print("Highest mark: ", scores.max())
-print("Lowest mark: ", scores.min())
-print("Adding 5 to every mark: ", scores + 5)
-print("Multiply all marks by 2: ", scores * 2)
+# for row in scores:
+#     print("axis=1", row)
+    
+# for col in scores.T:
+#     print("axis=0", col)
+
+# let's find out Average of each student with axis:
+
+print("Average mark of each student: ", scores.mean(axis=1))
+print("Average marks of each subject: ", scores.mean(axis=0))
 
 
 
