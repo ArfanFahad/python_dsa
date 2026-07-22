ae4aae87c88ada5d4b4b7c7053a0503278126e4d
fahad <fahad@debian.fahad>
2026-07-22 12:36:57 +0600

    practice

--- pandas/data_types.py
+++ pandas/data_types.py
@@ -5,14 +5,17 @@
 
 import pandas as pd 
 
-df = pd.DataFrame({
-    "Name": ['Alice', 'Bob', 'Charlie'],
-    "Age": [25, 30, 50],
-    "Salary": [50000, 60000, 70000],
-    "Designation": ['Sr. Executive ', 'Manager', 'Executive Director'],
-    "Bonus": [2000, 4000, 5000]
-})
+sales = pd.Series(
+    [230, 440, 100],
+    index=["Sun", "Mon", "Tue"]
+)
 
-print(df)
-print("-------------------")
-print(df[df["Age"] < 50])
+print(sales)
+
+print("---------------------------------")
+
+print("Best Sales amount: ", sales.max())
+print("Worst Sales amount: ", sales.min())
+print("Average Sales amount: ", sales.mean())
+
+print("Monday sales: ", sales["Mon"])
